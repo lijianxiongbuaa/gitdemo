8878abf64c33a4c6d0d5984a01ee588ca89f6d7e
Li Jianxiong <1625004060@qq.com>
2020-03-22 15:18:43 +0800

    update fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -3,3 +3,23 @@
 #include<Windows.h>
 #define MAX_DIGITS 10
 #define CLEAR "cls"
+const int segments[10][7] = {
+    {1, 1, 1, 1, 1, 1, 0}, // code for 0 
+    {0, 1, 1, 0, 0, 0, 0}, // code for 1 
+    {1, 1, 0, 1, 1, 0, 1}, // code for 2 
+    {1, 1, 1, 1, 0, 0, 1}, // code for 3 
+    {0, 1, 1, 0, 0, 1, 1}, // code for 4 
+    {1, 0, 1, 1, 0, 1, 1}, // code for 5 
+    {1, 0, 1, 1, 1, 1, 1}, // code for 6 
+    {1, 1, 1, 0, 0, 0, 0}, // code for 7 
+    {1, 1, 1, 1, 1, 1, 1}, // code for 8 
+    {1, 1, 1, 1, 0, 1, 1}  // code for 9 
+};
+
+char digits[3][MAX_DIGITS * 4];
+
+void clear_digits_array(void);//flush the canvas
+void process_digits_array(int dight, int position);//paint
+void print_digits_array(void);//print 
+int display(int);//print an int 
+void countdown_display(int);//countdown based on display
