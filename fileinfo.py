09438c53f41d92c2b34a4399929e050556509d7c
Li Jianxiong <1625004060@qq.com>
2020-03-22 15:27:13 +0800

    initial update

--- fileinfo.py
+++ fileinfo.py
@@ -46,3 +46,35 @@ void clear_digits_array(void)
     for(j=0;j<MAX_DIGITS*4;j++)
         digits[i][j]=' ';
 }
+void process_digits_array(int digit, int position) 
+{
+	if(position>=MAX_DIGITS)
+		position=position-MAX_DIGITS;
+	int n = position * 4;
+    if (segments[digit][0])
+        digits[0][n + 1] = '_';
+    if (segments[digit][1])
+        digits[1][n + 2] = '|';
+    if (segments[digit][2])
+        digits[2][n + 2] = '|';
+    if (segments[digit][3])
+        digits[2][n + 1] = '_';
+    if (segments[digit][4])
+        digits[2][n] = '|';
+    if (segments[digit][5])
+        digits[1][n] = '|';
+    if (segments[digit][6])
+        digits[1][n + 1] = '_';
+}
+
+void print_digits_array(void) 
+{
+    int i, j;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < MAX_DIGITS * 4; j++) 
+        {
+            printf("\033[40;31m%c\033[0m",digits[i][j]);    //输出红色字体 
+        }
+        printf("\n");
+    }
+}
