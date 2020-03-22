9d88575b9b8cf7b31ed42b83ec84b3fa9e7b2932
Li Jianxiong <1625004060@qq.com>
2020-03-22 15:25:23 +0800

    initial update

--- fileinfo.py
+++ fileinfo.py
@@ -23,3 +23,26 @@ void process_digits_array(int dight, int position);//paint
 void print_digits_array(void);//print 
 int display(int);//print an int 
 void countdown_display(int);//countdown based on display
+
+int main(void) 
+{
+    printf("Enter a positive integer: ");
+    int n;
+    scanf("%d",&n);
+    if(n<=0)
+    {
+        printf("ERROR INPUT!\n");
+        return -1;
+    } 
+    printf("%d can be displayed as:\n",n);
+    //return display(n);
+    countdown_display(n);
+}
+
+void clear_digits_array(void) 
+{
+   int i,j;
+   for(i=0;i<3;i++)
+    for(j=0;j<MAX_DIGITS*4;j++)
+        digits[i][j]=' ';
+}
